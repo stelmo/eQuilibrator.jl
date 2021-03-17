d15cb787b0f83dc2dc85804e5bcfb4c6b3b0e504
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-17 09:41:04 +0100

    add docs building

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, Equilibrator
+
+makedocs(modules = [Equilibrator],
+    clean = false,
+    format = Documenter.HTML(prettyurls = !("local" in ARGS),
+             canonical = "https://stelmo.github.io/Equilibrator.jl/stable/"),
+    sitename = "Equilibrator.jl",
+    authors = "The developers of Equilibrator.jl",
+    linkcheck = !("skiplinks" in ARGS),
+    pages = [
+        "Documentation" => "index.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/stelmo/Equilibrator.jl.git",
+    target = "build",
+    branch = "gh-pages",
+    push_preview = true
+)
