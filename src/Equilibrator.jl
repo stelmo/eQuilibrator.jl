b2381849a5d3bd5b318989df7f71eea2a4f0d37f
St. Elmo <stelmozors@gmail.com>
2021-05-29 16:22:43 +0200

    update

--- src/Equilibrator.jl
+++ src/Equilibrator.jl
@@ -2,7 +2,9 @@ module eQuilibrator
 
 using PyCall
 
-include("init_functions.jl")
+# initialize
+include("init.jl")
+# useful functions
 include("tools.jl")
 
 export build_rxn_strings, map_gibbs_rxns, map_gibbs_external, map_gibbs_internal
